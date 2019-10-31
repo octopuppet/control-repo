438ddf95d8a9934b10dc300ddbe3f91edcf4a482
octopuppet <root@puppet.tspi.local>
2019-10-31 14:21:43 -0400

    shifted firewall declarations to base profile

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -2,4 +2,17 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 
+    resources { 'firewall':
+        purge => true,
+    }
+
+    Firewall {
+        before  => Class['my_firewall::post'],
+        require => Class['my_firewall::pre'],
+    }
+
+    class { ['my_firewall::pre', 'my_firewall::post']: }
+
+    class { 'firewall': }
+
 }
