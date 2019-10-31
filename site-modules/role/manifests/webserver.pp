1e69c32f1afd17e85e245f48cfd1835ec5da678a
octopuppet <root@puppet.tspi.local>
2019-10-31 16:39:54 -0400

    just testing webhooks

--- site-modules/role/manifests/webserver.pp
+++ site-modules/role/manifests/webserver.pp
@@ -1,7 +1,7 @@
 class role::webserver {
 
   #This role would be made of all the profiles that need to be included to make a webserver work
-  #All roles should include the base profile
+  #All roles should include the base
   include profile::base
   include profile::web
 }
