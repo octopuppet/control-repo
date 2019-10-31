19bfc04641023433fece3fdf383ff9c741a69f62
octopuppet <root@puppet.tspi.local>
2019-10-31 13:56:40 -0400

    rewrote web profile

--- site-modules/profile/manifests/web.pp
+++ site-modules/profile/manifests/web.pp
@@ -1,15 +1,30 @@
 class profile::web {
-	
-	class { 'apache':
-		# this is where parameter definitions would go
-		default_mods => true,
-	}
-    firewall { '100 allow http and https access':
-        dport  => [80, 443],
-        proto  => tcp,
-        action => accept,
+
+    firewall { '100 allow httpd':
+        chain   => 'INPUT',
+        state   => ['NEW'],
+        dport   => '80',
+        proto   => 'tcp',
+        action  => 'accept',
+    }  
+  
+    package { 'httpd':
+        ensure  => present,
+        before  => File['/etc/httpd/conf/httpd.conf'],
     }
+  
+    file { '/etc/httpd/conf/httpd.conf':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        source  => "puppet:///modules/httpd/httpd.conf",
+    }
+  
+    service { 'httpd':
+        ensure     => running,
+        enable     => true,
+        subscribe  => File['/etc/httpd/conf/httpd.conf'],
+    }
+
 }
-
-
-	
